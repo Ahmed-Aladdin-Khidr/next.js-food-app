f4af0b04970348ed882f49372e8a816b6f25eeaa
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-04-20 21:30:24 +0200

    Add ShareMealPage component with form for meal sharing and image upload
    Adds ShareMealPage component
    
    Introduces the ShareMealPage component with a form
    for users to share their favorite meals.
    
    Includes fields for meal details, instructions,
    and image upload functionality.
    
    Created an image picker component with image preview.

---
+++ components/meals/image-picker.js
@@ -0,0 +1,60 @@
+"use client";
+import Image from "next/image";
+import classes from "./image-picker.module.css";
+import { useRef, useState } from "react";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const pickReference = useRef();
+
+  const handlePickClick = () => {
+    pickReference.current.click();
+  };
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image chosen yet!</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={pickReference}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
